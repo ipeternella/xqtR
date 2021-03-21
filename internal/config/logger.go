1b2feaec905abbc68bdec757b334642ca2030f7f
IgooorGP <igor.feq@gmail.com>
2021-03-21 12:15:47 -0300

    add log-level validation

---
+++ internal/config/logger.go
@@ -0,0 +1,11 @@
+package config
+
+import "github.com/rs/zerolog"
+
+var StrToLogLevelMapping = map[string]zerolog.Level{
+	"debug": zerolog.DebugLevel,
+	"info":  zerolog.InfoLevel,
+	"warn":  zerolog.WarnLevel,
+	"error": zerolog.ErrorLevel,
+	"fatal": zerolog.FatalLevel,
+}
--- pkg/cmd/install.go
+++ pkg/cmd/install.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/IgooorGP/xqtR/internal/config"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
@@ -11,13 +14,24 @@ func newRunCmd(cfg *config.XqtRConfig) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "run",
 		Short: "Installs all software found in an Minstall file.",
+		Args: func(cmd *cobra.Command, args []string) error {
+			// checks for acceptable log level
+			_, okLogLevel := config.StrToLogLevelMapping[cfg.LogLevel]
+
+			if !okLogLevel {
+				return errors.New(fmt.Sprintf("Unknown log level: %s", cfg.LogLevel))
+			}
+
+			return nil
+		},
 		Run: func(cmd *cobra.Command, args []string) {
 			log.Info().Msg("Running...")
 		},
 	}
 
-	cmd.PersistentFlags().StringVarP(&cfg.JobFilePath, "file", "f", "minstall.yml", "The install file path")
-	cmd.PersistentFlags().StringVar(&cfg.LogLevel, "log-level", "info", "Logging verbosity")
+	// persistent flags -> cascades to subcommands
+	cmd.PersistentFlags().StringVarP(&cfg.JobFilePath, "file", "f", "job.yml", "The install file path")
+	cmd.PersistentFlags().StringVar(&cfg.LogLevel, "log-level", "info", "Logging verbosity: ('debug', 'info', 'warn', 'error', 'fatal')")
 	cmd.PersistentFlags().BoolVar(&cfg.IsDryRun, "dry-run", false, "Runs the program without installing or downloading anything")
 
 	return cmd
