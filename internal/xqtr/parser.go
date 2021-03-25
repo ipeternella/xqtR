339d8cdc34bd6d9c3a63c095b37ebef71e675c42
IgooorGP <igor.feq@gmail.com>
2021-03-24 21:23:02 -0300

    add yaml schema validation on parser

--- internal/xqtr/parser.go
+++ internal/xqtr/parser.go
@@ -1,6 +1,8 @@
 package xqtr
 
 import (
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 
@@ -40,5 +42,45 @@ func parseYaml(viperParser *viper.Viper, absJobsFilePath string) *dtos.JobsFile 
 		panic(err)
 	}
 
+	// yaml schema validation
+	if err := validateYamlSchema(yaml); err != nil {
+		log.Fatal().Msgf("yaml structure error: %s", err.Error()) // status code 1
+	}
+
 	return yaml
+}
+
+func validateYamlSchema(yaml *dtos.JobsFile) error {
+	jobNum := 0
+
+	if len(yaml.Jobs) == 0 {
+		return errors.New(`missing "jobs" key on yaml file`)
+	}
+
+	for _, job := range yaml.Jobs {
+		jobNum++     // increment job number to help the user find the job in case of errors
+		stepNum := 0 // for steps errors
+
+		if job.Title == "" {
+			return fmt.Errorf(`missing "jobs" key on yaml file (job number: %d)`, jobNum)
+		}
+
+		if len(job.Steps) == 0 {
+			return fmt.Errorf(`missing "steps" key for (job name: "%s", job number: %d)`, job.Title, jobNum)
+		}
+
+		for _, step := range job.Steps {
+			stepNum++ // increment step number to help the user find the step in case of errors
+
+			if step.Name == "" {
+				return fmt.Errorf(`missing "name" key for step on (job name: "%s", job number: %d, step number: %d)`, job.Title, jobNum, stepNum)
+			}
+
+			if step.Run == "" {
+				return fmt.Errorf(`missing "run" key for step on (job name: "%s", job number: %d, step name: "%s", step number: %d)`, job.Title, jobNum, step.Name, stepNum)
+			}
+		}
+	}
+
+	return nil
 }
