d76d93805ed84ff7e8d469c1c821ffe86321cc82
IgooorGP <igor.feq@gmail.com>
2021-03-25 10:50:15 -0300

    add more yaml schema validation tests

--- internal/xqtr/parser_test.go
+++ internal/xqtr/parser_test.go
@@ -13,7 +13,7 @@ const (
 
 func TestShouldBreakWhenYamlHasMissingStepName(t *testing.T) {
 	// arrange
-	brokenYamlRelativePath := brokenYamlFolderPath + "broken_no_step_name.yaml"
+	brokenYamlRelativePath := brokenYamlFolderPath + "broken_no_step_name.yaml" // missing name of a step on yaml
 	brokenYamlPath_noJobs, _ := filepath.Abs(brokenYamlRelativePath)
 
 	viperParser := prepareViper(brokenYamlPath_noJobs)
@@ -25,4 +25,52 @@ func TestShouldBreakWhenYamlHasMissingStepName(t *testing.T) {
 	// assert
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), `missing "name" key for step on (job name: "job 1", job number: 1, step number: 3)`)
+}
+
+func TestShouldBreakWhenYamlHasMissingStepRun(t *testing.T) {
+	// arrange
+	brokenYamlRelativePath := brokenYamlFolderPath + "broken_no_step_run.yaml" // missing run of a step on yaml
+	brokenYamlPath_noJobs, _ := filepath.Abs(brokenYamlRelativePath)
+
+	viperParser := prepareViper(brokenYamlPath_noJobs)
+	yaml := parseYaml(viperParser, brokenYamlPath_noJobs)
+
+	// act
+	err := validateYamlSchema(yaml)
+
+	// assert
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), `missing "run" key for step on (job name: "job 2", job number: 2, step name: "task 22", step number: 2)`)
+}
+
+func TestShouldBreakWhenYamlHasJobWithoutSteps(t *testing.T) {
+	// arrange
+	brokenYamlRelativePath := brokenYamlFolderPath + "broken_job_without_steps.yaml" // job with just a "title" but no "steps" list
+	brokenYamlPath_noJobs, _ := filepath.Abs(brokenYamlRelativePath)
+
+	viperParser := prepareViper(brokenYamlPath_noJobs)
+	yaml := parseYaml(viperParser, brokenYamlPath_noJobs)
+
+	// act
+	err := validateYamlSchema(yaml)
+
+	// assert
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), `missing "steps" key for (job name: "job 2", job number: 2)`)
+}
+
+func TestShouldBreakWhenYamlHasStepNameTypo(t *testing.T) {
+	// arrange
+	brokenYamlRelativePath := brokenYamlFolderPath + "broken_step_run_typo.yaml" // job with just a "title" but no "steps" list
+	brokenYamlPath_noJobs, _ := filepath.Abs(brokenYamlRelativePath)
+
+	viperParser := prepareViper(brokenYamlPath_noJobs)
+	yaml := parseYaml(viperParser, brokenYamlPath_noJobs)
+
+	// act
+	err := validateYamlSchema(yaml)
+
+	// assert
+	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), `missing "run" key for step on (job name: "job 2", job number: 2, step name: "task 21", step number: 1)`)
 }
